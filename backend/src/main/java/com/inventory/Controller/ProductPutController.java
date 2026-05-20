b939301986aed582cd6c977178b8b15e8e455dc8
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-05-20 17:21:45 +0900

    feat: 商品情報を更新するAPIのControllerを追加

---
+++ backend/src/main/java/com/inventory/Controller/ProductPutController.java
@@ -0,0 +1,25 @@
+package com.inventory.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inventory.Entity.Product;
+import com.inventory.Service.ProductPutService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductPutController {
+    @Autowired
+    private ProductPutService productPutService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> replaceProduct(@Validated @RequestBody Product newProduct, @PathVariable Integer id) {
+        return productPutService.replaceProduct(newProduct, id);
+    }
+}
