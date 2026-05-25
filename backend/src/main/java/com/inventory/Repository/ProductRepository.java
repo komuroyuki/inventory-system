142bcfdc5a0cc4949f434310f9f684b3f5d16b5d
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-05-25 11:37:02 +0900

    feat: 商品テーブルのリポジトリに、商品名で検索するクエリメソッドを追加

--- backend/src/main/java/com/inventory/Repository/ProductRepository.java
+++ backend/src/main/java/com/inventory/Repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.inventory.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.inventory.Entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+    List<Product> findByNameContaining(String keyword);
 }
