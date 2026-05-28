cf6f4c57d7b68bf4f656dba7744e3cc542e040e5
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-05-28 13:09:37 +0900

    test: APIのテストから未使用の変数を削除

--- backend/src/test/java/com/inventory/backend/HttpRequestTests.java
+++ backend/src/test/java/com/inventory/backend/HttpRequestTests.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.client.RestTestClient;
 
 import com.inventory.DTO.ProductRequest;
@@ -18,9 +17,6 @@ import com.inventory.Repository.ProductRepository;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureRestTestClient
 class HttpRequestTests {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private RestTestClient restTestClient;
