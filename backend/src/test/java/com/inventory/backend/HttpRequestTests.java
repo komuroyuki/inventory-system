8b690b48483ab2731da8831cf751f9a78e65d8ce
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-05-28 11:30:00 +0900

    test: 商品情報を更新するAPIの異常系のテストを追加

--- backend/src/test/java/com/inventory/backend/HttpRequestTests.java
+++ backend/src/test/java/com/inventory/backend/HttpRequestTests.java
@@ -45,4 +45,91 @@ class HttpRequestTests {
         assertThat(product.getImage()).isEqualTo(productRequest.image());
         assertThat(product.getCategoryId().getId()).isEqualTo(productRequest.categoryId());
     }
+
+    @Test
+    void replaceProductShouldReturnErrorIfIdNotExist() {
+        ProductRequest productRequest = new ProductRequest("Error", 100, "error", 3);
+
+        restTestClient.put()
+                .uri("/products/1000")
+                .body(productRequest)
+                .exchange()
+                .expectStatus().isNotFound();
+
+        Product product = productRepository.findById(1000).orElse(null);
+
+        assertThat(product).isEqualTo(null);
+    }
+
+    @Test
+    void replaceProductShouldReturnErrorIfCategoryIdNotExist() {
+        ProductRequest productRequest = new ProductRequest("Error", 100, "error", 100);
+
+        restTestClient.put()
+                .uri("/products/1")
+                .body(productRequest)
+                .exchange()
+                .expectStatus().isBadRequest();
+
+        Product product = productRepository.findById(1).orElseThrow();
+
+        assertThat(product.getName()).isNotEqualTo(productRequest.name());
+        assertThat(product.getQuantity()).isNotEqualTo(productRequest.quantity());
+        assertThat(product.getImage()).isNotEqualTo(productRequest.image());
+        assertThat(product.getCategoryId().getId()).isNotEqualTo(productRequest.categoryId());
+    }
+
+    @Test
+    void replaceProductShouldReturnErrorIfNameIsBlank() {
+        ProductRequest productRequest = new ProductRequest("", 100, "error", 3);
+
+        restTestClient.put()
+                .uri("/products/1")
+                .body(productRequest)
+                .exchange()
+                .expectStatus().isBadRequest();
+
+        Product product = productRepository.findById(1).orElseThrow();
+
+        assertThat(product.getName()).isNotEqualTo(productRequest.name());
+        assertThat(product.getQuantity()).isNotEqualTo(productRequest.quantity());
+        assertThat(product.getImage()).isNotEqualTo(productRequest.image());
+        assertThat(product.getCategoryId().getId()).isNotEqualTo(productRequest.categoryId());
+    }
+
+    @Test
+    void replaceProductShouldReturnErrorIfQuantityIsNegative() {
+        ProductRequest productRequest = new ProductRequest("Error", -1, "error", 3);
+
+        restTestClient.put()
+                .uri("/products/1")
+                .body(productRequest)
+                .exchange()
+                .expectStatus().isBadRequest();
+
+        Product product = productRepository.findById(1).orElseThrow();
+
+        assertThat(product.getName()).isNotEqualTo(productRequest.name());
+        assertThat(product.getQuantity()).isNotEqualTo(productRequest.quantity());
+        assertThat(product.getImage()).isNotEqualTo(productRequest.image());
+        assertThat(product.getCategoryId().getId()).isNotEqualTo(productRequest.categoryId());
+    }
+
+    @Test
+    void replaceProductShouldReturnErrorIfCategoryIdIsNull() {
+        ProductRequest productRequest = new ProductRequest("Error", 100, "error", null);
+
+        restTestClient.put()
+                .uri("/products/1")
+                .body(productRequest)
+                .exchange()
+                .expectStatus().isBadRequest();
+
+        Product product = productRepository.findById(1).orElseThrow();
+
+        assertThat(product.getName()).isNotEqualTo(productRequest.name());
+        assertThat(product.getQuantity()).isNotEqualTo(productRequest.quantity());
+        assertThat(product.getImage()).isNotEqualTo(productRequest.image());
+        assertThat(product.getCategoryId().getId()).isNotEqualTo(productRequest.categoryId());
+    }
 }
