d072734220c397422cd5cfbc02a4807f420edd6b
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-06-02 17:51:22 +0900

    feat: 商品テーブルに商品を追加するAPIのServiceを追加

---
+++ backend/src/main/java/com/inventory/Service/ProductPostService.java
@@ -0,0 +1,42 @@
+package com.inventory.Service;
+
+import java.util.Objects;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.inventory.DTO.ProductRequest;
+import com.inventory.Entity.Category;
+import com.inventory.Entity.Product;
+import com.inventory.Repository.CategoryRepository;
+import com.inventory.Repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductPostService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ResponseEntity<?> postProduct(ProductRequest request) {
+
+        Category category = categoryRepository.findById(Objects.requireNonNull(request.categoryId())).orElse(null);
+
+        if (category == null) {
+            return ResponseEntity.badRequest().body("カテゴリIDが存在しません。" + System.lineSeparator());
+        }
+
+        Product product = new Product();
+
+        product.setName(request.name());
+        product.setQuantity(request.quantity());
+        product.setImage(request.image());
+        product.setCategoryId(category);
+
+        Product newProduct = productRepository.save(product);
+
+        return ResponseEntity.ok(newProduct);
+    }
+}
