f3ffe9df9be052375c8d972418a0d9d2c0616ea7
nosaka.kosuke@impl.co.jp <implmbp142@implMBP142.local>
2026-06-05 12:01:33 +0900

    Security

--- backend/src/main/java/com/inventory/Controller/ProductPostController.java
+++ backend/src/main/java/com/inventory/Controller/ProductPostController.java
@@ -2,7 +2,7 @@ package com.inventory.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class ProductPostController {
 
     private final ProductPostService service;
 
-    @PreAuthorize("hasRole('ADMIN')")
+    // @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/products")
     public ResponseEntity<ProductResponse> postProduct(@RequestBody @Valid ProductRequest request) {
 
