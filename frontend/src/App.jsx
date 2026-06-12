e7ae235704b14aa2a633cb930e66547f23b2fe09
implMBP143 <implmbp143@implMBP143.local>
2026-06-12 09:09:52 +0900

    ログイン画面完成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import Top from './Top.jsx'
 import Product_details from './jsx/product_details.jsx'
+import Login from './Login.jsx'
 // 💡 1. 商品追加画面（AddProduct）をインポートする
 // (※ファイルの置き場所に合わせて、パスは適宜調整してください。ここではTop.jsxと同じ階層にあると仮定しています)
 import AddProduct from './AddProduct.jsx'
@@ -11,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Top />} />
         <Route path="/product/:productId" element={<Product_details />} />
         <Route path="/AddProduct" element={<AddProduct />} />
