341344421aa0b41f1d2ef2a8309ca9269a156a12
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-06-15 16:21:08 +0900

    refactor: ユーザーRepositoryから不要なアノテーションを削除

--- backend/src/main/java/com/inventory/Repository/UserRepository.java
+++ backend/src/main/java/com/inventory/Repository/UserRepository.java
@@ -1,13 +1,10 @@
 package com.inventory.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.inventory.Entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
     User findByEmail(String email);
-    
 }
