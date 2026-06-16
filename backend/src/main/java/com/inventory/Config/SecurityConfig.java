ddfa608749f77d02e578ec6fc79913080a0aa945
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-06-16 15:44:21 +0900

    feat: CorsConfigurationSourceにAuthorizationとContent-Typeのヘッダーを許可する設定を追加

--- backend/src/main/java/com/inventory/Config/SecurityConfig.java
+++ backend/src/main/java/com/inventory/Config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
