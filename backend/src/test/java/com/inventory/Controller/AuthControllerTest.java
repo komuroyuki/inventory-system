08a42731bae0591e24cf67ca8d6c5e7ef7cc9ba6
hirata.kaiki <hirata.kaiki@impl.co.jp>
2026-06-18 16:51:37 +0900

    test: 認証機能の正常系テストを追加

---
+++ backend/src/test/java/com/inventory/Controller/AuthControllerTest.java
@@ -0,0 +1,73 @@
+package com.inventory.Controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.inventory.DTO.LoginRequest;
+import com.inventory.DTO.LoginResponse;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Value("${jwt.secret}")
+    private String secretString;
+
+    @Test
+    void loginShouldReturnUserJwtWhenUserRequests() {
+        testLogin("user@example.com", "user");
+    }
+
+    @Test
+    void loginShouldReturnAdminJwtWhenAdminRequests() {
+        testLogin("admin@example.com", "admin");
+    }
+
+    private void testLogin(String email, String role) {
+        LoginRequest request = new LoginRequest();
+        request.setEmail(email);
+        request.setPassword("password");
+
+        client.post().uri("/products/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(LoginResponse.class)
+                .consumeWith(result -> assertLoginResponse(result, email, role));
+    }
+
+    private void assertLoginResponse(EntityExchangeResult<LoginResponse> result, String email, String role) {
+        LoginResponse body = result.getResponseBody();
+
+        SecretKey key = Keys.hmacShaKeyFor(secretString.getBytes(StandardCharsets.UTF_8));
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(body.getAccess_token())
+                .getBody();
+
+        assertThat(claims.getSubject()).isEqualTo(email);
+        assertThat(claims.get("role", String.class)).isEqualTo(role);
+    }
+
+}
