49739d957e89ec931f0ce9ef9b1d3bf912961023
implMBP205 <implmbp205@implMBP205s-MacBook-Pro.local>
2026-06-19 13:02:34 +0900

    トークン失効までの時間変更・トークン失効後の401返却

--- backend/src/main/java/com/inventory/Security/JwtUtil.java
+++ backend/src/main/java/com/inventory/Security/JwtUtil.java
@@ -16,7 +16,7 @@ public class JwtUtil {
     private String secretKey;
     private Key key;
 
-    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+    private final long EXPIRATION_TIME = 1000 * 60 * 15;
 
     @jakarta.annotation.PostConstruct
     public void init() {
